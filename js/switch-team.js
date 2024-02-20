30b08474bff96f659336d5b234ec087f37f9a439
Evgeniy-Popov-98 <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-02-20 14:18:36 +0200

    add-finish_topicVebsait

--- js/switch-team.js
+++ js/switch-team.js
@@ -1,16 +1,21 @@
 'use strick';
 const topicMain = document.querySelector('body');
 const switchButton = document.querySelector('.button-war');
-// const logoIcon = document.querySelectorAll('.logo');
-// const socialIcons = document.querySelectorAll('.social-icons');
-// const achievementIcons = document.querySelectorAll('#achievement-icons');
-// const footerIcon = document.querySelectorAll('.footer-img');
-// const modalIcons = document.querySelectorAll('.modal-info-svg');
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  topicMain.classList.add('dark');
+} else {
+  topicMain.classList.add('light');
+}
 
 switchButton.addEventListener('click', switchTeam);
 
 function switchTeam() {
-  if (condition) {
+  if (topicMain.classList.value === 'dark') {
+    topicMain.classList.remove('dark');
+    topicMain.classList.add('light');
   } else {
+    topicMain.classList.remove('light');
+    topicMain.classList.add('dark');
   }
 }
