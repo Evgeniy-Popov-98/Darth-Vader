6e45d0e4e098dc802d8c944cc8cf594c6a68d45c
Evgeniy-Popov-98 <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-02-20 13:33:12 +0200

    add-topicMain

--- js/switch-team.js
+++ js/switch-team.js
@@ -1,57 +1,16 @@
 'use strick';
-const teamMain = document.querySelector('body');
+const topicMain = document.querySelector('body');
 const switchButton = document.querySelector('.button-war');
-const logoIcon = document.querySelectorAll('.logo');
-const socialIcons = document.querySelectorAll('.social-icons');
-const achievementIcons = document.querySelectorAll('#achievement-icons');
-const footerIcon = document.querySelectorAll('.footer-img');
-const modalIcons = document.querySelectorAll('.modal-info-svg');
+// const logoIcon = document.querySelectorAll('.logo');
+// const socialIcons = document.querySelectorAll('.social-icons');
+// const achievementIcons = document.querySelectorAll('#achievement-icons');
+// const footerIcon = document.querySelectorAll('.footer-img');
+// const modalIcons = document.querySelectorAll('.modal-info-svg');
+
 switchButton.addEventListener('click', switchTeam);
 
-function switchTeam(event) {
-  if (teamMain.classList.contains('dark')) {
-    teamMain.classList.replace('dark', 'light');
+function switchTeam() {
+  if (condition) {
   } else {
-    teamMain.classList.replace('light', 'dark');
-  }
-
-  for (const item of logoIcon) {
-    if (item.classList.contains('visually-hidden')) {
-      item.classList.replace('visually-hidden', 'is-open');
-    } else if (item.classList.contains('is-open')) {
-      item.classList.replace('is-open', 'visually-hidden');
-    }
-  }
-
-  for (let index = 0; index < socialIcons.length; index++) {
-    if (socialIcons[index].classList.contains('visually-hidden')) {
-      socialIcons[index].classList.replace('visually-hidden', 'is-open');
-    } else if (socialIcons[index].classList.contains('is-open')) {
-      socialIcons[index].classList.add('visually-hidden');
-    }
-  }
-
-  for (let index = 0; index < achievementIcons.length; index++) {
-    if (achievementIcons[index].classList.contains('visually-hidden')) {
-      achievementIcons[index].classList.replace('visually-hidden', 'is-open');
-    } else if (achievementIcons[index].classList.contains('is-open')) {
-      achievementIcons[index].classList.add('visually-hidden');
-    }
-  }
-
-  for (const item of footerIcon) {
-    if (item.classList.contains('visually-hidden')) {
-      item.classList.replace('visually-hidden', 'is-open');
-    } else if (item.classList.contains('is-open')) {
-      item.classList.replace('is-open', 'visually-hidden');
-    }
-  }
-
-  for (const item of modalIcons) {
-    if (item.classList.contains('visually-hidden')) {
-      item.classList.replace('visually-hidden', 'is-open');
-    } else if (item.classList.contains('is-open')) {
-      item.classList.replace('is-open', 'visually-hidden');
-    }
   }
 }
